b45c7fc5a329b6029e3930652e21b8974ff33751
Bo-Shang-Chao <seanbopo@gmail.com>
2022-03-31 09:52:53 +0800

    add detail route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   //1. 聲明瞭一個私有 heroService 屬性，2. 把它標記為一個 HeroService 的注入點。
   constructor(private heroService: HeroService, private messageService: MessageService) { }
@@ -25,10 +24,5 @@ export class HeroesComponent implements OnInit {
     //等待 Observable 發出這個英雄陣列，這可能立即發生，也可能會在幾分鐘之後。 然後，subscribe() 方法把這個英雄陣列傳給這個回呼(Callback)函式，該函式把英雄陣列賦值給元件的 heroes 屬性。
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
     // this.heroes = this.heroService.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
